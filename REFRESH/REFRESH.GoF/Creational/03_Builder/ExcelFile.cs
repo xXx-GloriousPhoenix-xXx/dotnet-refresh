9158bcc1575b513568d591994a38b45d094edbe4
RomanPR <prikhodjko.roman@gmail.com>
2026-06-09 11:48:03 +0300

    Builder

---
+++ REFRESH/REFRESH.GoF/Creational/03_Builder/ExcelFile.cs
@@ -0,0 +1,6 @@
+﻿namespace REFRESH.GoF.Creational._03_Builder;
+
+public class ExcelFile(List<string> content)
+{
+    public List<string> Content { get; } = content;
+}
---
+++ REFRESH/REFRESH.GoF/Creational/03_Builder/IReportBuilder.cs
@@ -0,0 +1,7 @@
+﻿namespace REFRESH.GoF.Creational._03_Builder;
+public interface IReportBuilder
+{
+    IReportBuilder SetTitle(string title);
+    IReportBuilder AddText(string text);
+    IReportBuilder AddTable(string[] headers, string[][] rows);
+}
