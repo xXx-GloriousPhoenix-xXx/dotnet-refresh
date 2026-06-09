d041dd1a34fef03a27114ca94c971d2a0adcf5c6
RomanPR <prikhodjko.roman@gmail.com>
2026-06-09 12:33:44 +0300

    Prototype

---
+++ REFRESH/REFRESH.GoF/Creational/04_Prototype/Shape.cs
@@ -0,0 +1,8 @@
+﻿namespace REFRESH.GoF.Creational._04_Prototype;
+public abstract class Shape(int x, int y, string color)
+{
+    protected int _x = x;
+    protected int _y = y;
+    protected string _color = color;
+    public abstract Shape Clone();
+}
