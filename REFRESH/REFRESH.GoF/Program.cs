3a002d6aff011e1688b45bf85a6580b9f57f59da
RomanPR <prikhodjko.roman@gmail.com>
2026-06-10 12:07:38 +0300

    Bridge

--- REFRESH/REFRESH.GoF/Program.cs
+++ REFRESH/REFRESH.GoF/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using REFRESH.GoF.Structural._02_Bridge;
+
+var tv = new TV();
+var remote = new AdvancedRemote(tv);
+remote.Mute();
---
+++ REFRESH/REFRESH.GoF/Structural/02_Bridge/Remote.cs
@@ -0,0 +1,48 @@
+﻿namespace REFRESH.GoF.Structural._02_Bridge;
+public class Remote(IDevice device)
+{
+    protected readonly IDevice _device = device;
+    public void TogglePower()
+    {
+        if (_device.IsEnabled())
+        {
+            _device.Disable();
+        }
+        else
+        {
+            _device.Enable();
+        }
+    }
+    public void VolumeDown()
+    {
+        var volume = _device.GetVolume();
+        if (volume > 0)
+        {
+            _device.SetVoume(volume - 1);
+        }
+        else
+        {
+            _device.SetVoume(0);
+        }
+    }
+    public void VolumeUp()
+    {
+        var volume = _device.GetVolume();
+        if (volume < 100)
+        {
+            _device.SetVoume(volume + 1);
+        }
+        else
+        {
+            _device.SetVoume(100);
+        }
+    }
+    public void ChannelDown()
+    {
+        _device.SetChannel(_device.GetChannel() - 1);
+    }
+    public void ChannelUp()
+    {
+        _device.SetChannel(_device.GetChannel() + 1);
+    }
+}
