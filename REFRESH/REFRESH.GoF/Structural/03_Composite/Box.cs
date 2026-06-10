6fbcd6d4efb0eb87fd1c30af42b704d2dbf4d9d9
RomanPR <prikhodjko.roman@gmail.com>
2026-06-10 12:35:22 +0300

    Composite

---
+++ REFRESH/REFRESH.GoF/Structural/03_Composite/Box.cs
@@ -0,0 +1,19 @@
+﻿namespace REFRESH.GoF.Structural._03_Composite;
+
+public class Box : INode
+{
+    private readonly List<INode> _nodes = [];
+    public IReadOnlyCollection<INode> Nodes => _nodes;
+    public void AddNode(INode node)
+    {
+        _nodes.Add(node);
+    }
+    public void RemoveNode(INode node)
+    {
+        _nodes.Remove(node);
+    }
+    public decimal Total()
+    {
+        return _nodes.Sum(n => n.Total());
+    }
+}
