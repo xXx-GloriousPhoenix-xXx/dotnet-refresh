b6d811370b2167d491dfad0e471e99941a7481c0
RomanPR <prikhodjko.roman@gmail.com>
2026-06-12 22:49:43 +0300

    Iterator

---
+++ REFRESH/REFRESH.GoF/Behavioral/03_Iterator/Profile.cs
@@ -0,0 +1,7 @@
+﻿namespace REFRESH.GoF.Behavioral._03_Iterator;
+
+public class Profile(string email)
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public string Email { get; init; } = email;
+}
