9e6d62ee7ecffde9ed5275ae575492e165ebb95b
RomanPR <prikhodjko.roman@gmail.com>
2026-06-13 23:41:11 +0300

    Template Method

---
+++ REFRESH/REFRESH.GoF/Behavioral/09_TemplateMethod/OrcsAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace REFRESH.GoF.Behavioral._09_TemplateMethod;
+
+public class OrcsAI : GameAI
+{
+    private readonly List<Scout> _scouts = [];
+    private readonly List<Warrior> _warriors = [];
+    public override void BuildStructures()
+    {
+        
+    }
+    public override void BuildUnits()
+    {
+        
+    }
+    public override void SendScouts(Vector2 position)
+    {
+        if (_scouts.Count > 0)
+        {
+
+        }
+    }
+    public override void SendWarriors(Vector2 position)
+    {
+        if (_warriors.Count > 5)
+        {
+
+        }
+    }
+}
