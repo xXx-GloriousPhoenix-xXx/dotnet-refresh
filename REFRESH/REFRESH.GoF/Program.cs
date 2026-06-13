a9af55746d7efe40a72c6798f0f369e9f84799d8
RomanPR <prikhodjko.roman@gmail.com>
2026-06-14 01:03:37 +0300

    Program.cs Removed

--- REFRESH/REFRESH.GoF/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using REFRESH.GoF.Behavioral._10_Visitor;
-
-var app = new Application(new XMLExportVisitor());
-var shape = new CompoundShape();
-shape.Add(new Dot());
-shape.Add(new Circle());
-shape.Add(new Rectangle());
-app.Add(shape);
-app.Export();
